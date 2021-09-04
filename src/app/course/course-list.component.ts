be47e481eaf387208e54216e2b2eade3b408b573
Davi-Arauj <moreiradaavi@gmail.com>
2021-09-03 22:36:28 -0300

    Inicializando projeto em Angular

---
+++ src/app/course/course-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: './course-list.component.html'   
+})
+export class CourseListComponent{
+    
+}
