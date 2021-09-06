b208012390eb1aef1b0a4a08a788d4b9ab34036f
Davi-Arauj <moreiradaavi@gmail.com>
2021-09-05 22:41:27 -0300

    List Courses, Details Courses

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { NavBarComponent } from 'src/nav-bar/nav-bar.component';
+import { StarComponent } from 'src/star/star.component';
 
 import { AppComponent } from './app.component';
+import { CourseInfoComponent } from './course/course-info.component';
 import { CourseListComponent } from './course/course-list.component';
+import { Erro404Component } from './erro404/erro404.component';
+import { ReplacePipe } from './pipe/replace.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent
+    CourseListComponent,
+    StarComponent,
+    ReplacePipe,
+    NavBarComponent,
+    Erro404Component,
+    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: 'course/info/:id', component: CourseInfoComponent
+      },
+      {
+        path: '**', component: Erro404Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
